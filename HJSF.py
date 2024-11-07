84aee69594c1d5a75f563074ba47e7df4c9e5f9f
Valarie Dominguez <vad230001@utdallas.edu>
2024-11-07 13:09:38 -0600

    Added paddles and classes for paddles. THIS IS A 2 PLAYER GAME.

--- HJSF.py
+++ HJSF.py
@@ -1,16 +1,17 @@
 import pygame
+
 pygame.init()
 
 
 # Set up window
 WIDTH, HEIGHT = 800, 600
-wn = pygame.display.set_mode((WIDTH, HEIGHT))
+WN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Holly Jolly Snowball Fight!")
 
 FPS = 60
 
 WHITE = (255, 255, 255)
-BLACK = (255, 255, 255)
+BLACK = (0, 0, 0)
 ALPHA = (255, 255, 255, 255)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
@@ -18,39 +19,60 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
 class Paddle:
     COLOR = WHITE
+    VEL = 4
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
-    def draw(self, wn):
-        pygame.draw.rectangle(wn, self.COLOR, (self.x, self.y, self.width, self.height)))
+    def draw(self, WN):
+        pygame.draw.rect(
+            WN, self.COLOR, (self.x, self.y, self.width, self.height))
+    
+    def move (self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
 
 
-
-
-
-
-def draw(wn):
-    wn.fill(BLACK)
+def draw(WN):
+    WN.fill(BLACK)
     pygame.display.update()
     
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w]:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s]:
+        left_paddle.move(up=False)
+    
+    if keys[pygame.K_UP]:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN]:
+        right_paddle.move(up=False)
+
+
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH , HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
 
     while run:
         clock.tick(FPS)
-        draw(wn)
+        draw(WN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        keys = pygame.ket.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
 
