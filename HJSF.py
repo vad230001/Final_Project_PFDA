f790a559f8b402ed8dc706b111805f918645fa2a
Valarie Dominguez <vad230001@utdallas.edu>
2024-11-07 13:51:09 -0600

    Added Controller Borders BABYYYYYYYYYYYEAHH!!

--- HJSF.py
+++ HJSF.py
@@ -20,6 +20,7 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 class Paddle:
     COLOR = WHITE
     VEL = 4
+
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -27,8 +28,7 @@ class Paddle:
         self.height = height
 
     def draw(self, WN):
-        pygame.draw.rect(
-            WN, self.COLOR, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(WN, self.COLOR, (self.x, self.y, self.width, self.height))
     
     def move (self, up=True):
         if up:
@@ -37,19 +37,24 @@ class Paddle:
             self.y += self.VEL
 
 
-def draw(WN):
+def draw(WN, paddles):
     WN.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(WN)
+    
     pygame.display.update()
     
+# Setting movemnet keys and borders for paddles.
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
         left_paddle.move(up=True)
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
         left_paddle.move(up=False)
     
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
         right_paddle.move(up=True)
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and right_paddle.y - right_paddle.VEL + right_paddle.height <= HEIGHT:
         right_paddle.move(up=False)
 
 
@@ -59,12 +64,12 @@ def main():
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
-    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH , HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
 
     while run:
         clock.tick(FPS)
-        draw(WN)
+        draw(WN, [left_paddle, right_paddle])
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
